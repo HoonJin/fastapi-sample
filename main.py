2c571594fabbfcba625062eefd116335ff3a9f7b
Daniel Ji <bwjhj1030@gmail.com>
2020-04-02 18:51:39 +0900

    익셉션을 받아서 처리하는 커스텀 익셉션 핸들러를 만들고, 로그를 추가

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from config import conf
+from config import conf, ex_handlers
 from database import db
 from test import test_router
 
 app = FastAPI()
 app.include_router(test_router, tags=['tests'])
+
+app.add_exception_handler(HTTPException, ex_handlers.custom_http_exception_handler)
 
 
 @app.on_event("startup")
