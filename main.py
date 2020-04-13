5aeec07f8d1c316caf60c1c270492b09ea13e6ea
Daniel Ji <bwjhj1030@gmail.com>
2020-04-13 23:58:35 +0900

    logging 이 제대로 동작하지 않아 미들웨어 방식에서 디펜던시 방식으로 다시 변경

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 
 import uvicorn
-from fastapi import FastAPI, HTTPException, Response, Request
+from fastapi import FastAPI, HTTPException, Request, Depends
 
 from app.test import test_router
 from config import conf, ex_handlers
@@ -14,13 +14,25 @@ app = FastAPI(
     docs_url='/docs' if ENV == 'dev' else None,
     openapi_url='/openapi.json' if ENV == 'dev' else None,
 )
-app.include_router(test_router, tags=['tests'])
-
 app.add_exception_handler(HTTPException, ex_handlers.custom_http_exception_handler)
 
 
-@app.middleware("http")
-async def __request_logging(req: Request, call_next) -> Response:
+# @app.middleware("http")
+# async def __request_logging(req: Request, call_next: Callable) -> Response:
+#     request_id = req.headers.get('X-Request-ID', '')
+#     if req.method == "POST":
+#         request_info = f"body: {(await req.body()).decode('UTF-8')}"
+#     elif req.method == "PUT" or req.method == "DELETE":
+#         request_info = f"path_params: {req.path_params}, body: {(await req.body()).decode('UTF-8')}"
+#     else:  # GET
+#         request_info = f"query_params: {req.query_params}"
+#     logging.info(f"[{request_id}][{datetime.utcnow()}]<{req.method} {req.get('path')}> {request_info}")
+#
+#     res = await call_next(req)
+#     logging.info(f"[{request_id}][{datetime.utcnow()}] code: {res.status_code}, headers: {res.headers}")
+#     return res
+
+async def __request_logging(req: Request):
     request_id = req.headers.get('X-Request-ID', '')
     if req.method == "POST":
         request_info = f"body: {(await req.body()).decode('UTF-8')}"
@@ -28,10 +40,10 @@ async def __request_logging(req: Request, call_next) -> Response:
         request_info = f"path_params: {req.path_params}, body: {(await req.body()).decode('UTF-8')}"
     else:  # GET
         request_info = f"query_params: {req.query_params}"
-    logging.info(f"[{request_id}][{datetime.utcnow()}] {req.method} {req.get('path')} {request_info}")
-    res: Response = await call_next(req)
-    logging.info(f"[{request_id}][{datetime.utcnow()}] code: {res.status_code}, headers: {res.headers}")
-    return res
+    logging.info(f"[{request_id}][{datetime.utcnow()}]<{req.method} {req.get('path')}> {request_info}")
+
+
+app.include_router(test_router, tags=['tests'], dependencies=[Depends(__request_logging)])
 
 
 @app.on_event("startup")
@@ -57,4 +69,4 @@ async def forbidden():
 
 if __name__ == '__main__':
     LOG_LEVEL = conf.get('LOG_LEVEL', str, 'info')
-    uvicorn.run(app, host='127.0.0.1', port=8000, log_level=LOG_LEVEL)
+    uvicorn.run(app, host='127.0.0.1', port=8000, log_level=LOG_LEVEL, debug=True)
