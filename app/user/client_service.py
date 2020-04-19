d106a5455845de493e813a8bbd568c11575227b3
Daniel Ji <bwjhj1030@gmail.com>
2020-04-19 20:03:01 +0900

    jwt token 의 키들을 일반적으로 사용하는 이름으로 변경 & iss 키로 서비스에서 발급한 토큰인지, client key를가지고 발급한 것인지 여부를 따짐 & exception 쪽 일부 코드 정리

--- app/user/client_service.py
+++ app/user/client_service.py
@@ -1,16 +1,16 @@
+import logging
 import secrets
 import string
 from datetime import datetime, timedelta
 
 import jwt
 from cryptography.fernet import Fernet
-from jwt import InvalidTokenError
 
 import config
 from config.exceptions import BadRequestException, UnauthorizedException, ErrorCode
 from .client_dao import ClientDao
 from .domains import TokenResponse, LoginForm, ClientForm
-from .entities import User
+from .entities import User, Client
 from .user_dao import UserDao
 from .user_service import UserService
 
@@ -52,8 +52,8 @@ class ClientService:
             user = await UserDao.get_by_id(client.user_id)
             renew_time = datetime.utcnow() + timedelta(days=1)
             await ClientDao.update_expires_at(form.client_id, renew_time)
-            data = {'uuid': user.uuid, 'client_id': client.client_id, 'expires_at': renew_time.timestamp()}
-            access_token = jwt.encode(data, form.client_secret).decode()
+            payload = {'uuid': user.uuid, 'client_id': client.client_id, 'exp': renew_time.timestamp()}
+            access_token = jwt.encode(payload, form.client_secret).decode()
             return TokenResponse(access_token=access_token)
         else:
             raise UnauthorizedException
@@ -65,37 +65,47 @@ class ClientService:
             raise UnauthorizedException
 
         expires_at = (datetime.utcnow() + timedelta(days=1)).timestamp()
-        data = {'uuid': user.uuid, 'service': form.service, 'expires_at': expires_at}
-        access_token = jwt.encode(data, login_secret).decode()
+        payload = {'uuid': user.uuid, 'iss': form.service, 'exp': expires_at}
+        access_token = jwt.encode(payload, login_secret).decode()
         return TokenResponse(access_token=access_token)
 
     @staticmethod
+    async def get_validated_client(client_id: str) -> Client:
+        if client_id is None:
+            raise BadRequestException
+        client = await ClientDao.find_by_client_id(client_id)
+        if client is None:
+            raise BadRequestException(ErrorCode.invalid_client)
+        if not client.trusted:
+            raise UnauthorizedException(detail='신뢰할 수 없는 키')
+        return client
+
+    @staticmethod
     async def get_user_by_access_token(access_token: str):
+        # exp 의 경우 utc 로 설정했는데 인증할 때 kst 현재시간을 기준으로 처리해서 처리가 되지 않음
+        # 따라서 수동으로 처리
+        verify_options = {'verify_exp': False}
         if access_token is None:
             raise BadRequestException
-        unverified_data = jwt.decode(access_token, verify=False, algorithms=[_ALGORITHM])
-        client_id = unverified_data.get('client_id')
+        data = jwt.decode(access_token, verify=False, algorithms=[_ALGORITHM])
 
         try:
-            if client_id is None:  # 일반적인 로그인 경우
-                service = unverified_data.get('service')
-                if service is None:
-                    raise UnauthorizedException
-                data = jwt.decode(access_token, login_secret, algorithms=[_ALGORITHM])
+            issuer = data.get('iss')
+            if issuer is not None:
+                # TODO issuer 로 secret 키를 맵핑시켜서 decode 할 것
+                payload = jwt.decode(access_token, login_secret, algorithms=[_ALGORITHM], options=verify_options)
             else:  # access_token 사용 경우
-                client = await ClientDao.find_by_client_id(client_id)
-                if not client.trusted:
-                    raise UnauthorizedException('신뢰할 수 없는 키')
+                client = await ClientService.get_validated_client(data.get('client_id'))
                 secret = decrypt(client.encrypted_secret)
-                data = jwt.decode(access_token, secret, algorithms=[_ALGORITHM])
-        except InvalidTokenError as e:  # jwt 토큰 검증에 실패한 경우
-            raise UnauthorizedException(str(e))
+                payload = jwt.decode(access_token, secret, algorithms=[_ALGORITHM], options=verify_options)
+        except jwt.InvalidTokenError as e:  # jwt 토큰 검증에 실패한 경우
+            logging.error(e, exc_info=True)
+            raise UnauthorizedException(detail=str(e))
 
-        if data.get('expires_at') < datetime.utcnow().timestamp():
+        if payload.get('exp') < datetime.utcnow().timestamp():
             raise BadRequestException(ErrorCode.token_expired)
 
-        user_uuid = data.get('uuid')
-        user = await UserDao.find_by_uuid(user_uuid)
+        user = await UserDao.find_by_uuid(payload.get('uuid'))
         if user is None:
             raise BadRequestException
         return user
--- config/exceptions.py
+++ config/exceptions.py
@@ -8,7 +8,29 @@ from fastapi.responses import JSONResponse
 
 
 class ErrorCode(str, Enum):
+    # 400
+    bad_request = 'bad_request'
+    invalid_client = 'invalid_client'
+    invalid_parameter = 'invalid_parameter'
+
+    # 401
+    unauthorized = 'unauthorized'
     token_expired = 'token_expired'
+
+    # 403
+    forbidden = 'forbidden'
+
+    # 404
+    not_found = 'not_found'
+
+    # 422
+    unprocessable_entity = 'unprocessable_entity'
+
+    # 429
+    too_many_requests = 'too_many_requests'
+
+    # 500
+    internal_server_error = 'internal_server_error'
 
 
 class HTTPException(Exception):
@@ -27,38 +49,39 @@ class HTTPException(Exception):
 
 
 class BadRequestException(HTTPException):
-    def __init__(self, code='bad_request', detail: Any = None):
+    def __init__(self, code=ErrorCode.bad_request, detail: Any = None):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, code=code, detail=detail)
 
 
 class UnauthorizedException(HTTPException):
-    def __init__(self, code='unauthorized', detail: Any = None):
+    def __init__(self, code=ErrorCode.unauthorized, detail: Any = None):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, code=code, detail=detail)
 
 
 class ForbiddenException(HTTPException):
-    def __init__(self, code='forbidden', detail: Any = None):
+    def __init__(self, code=ErrorCode.forbidden, detail: Any = None):
         super().__init__(status_code=status.HTTP_403_FORBIDDEN, code=code, detail=detail)
 
 
 class NotFoundException(HTTPException):
-    def __init__(self, code='not_found', detail: Any = None):
+    def __init__(self, code=ErrorCode.not_found, detail: Any = None):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, code=code, detail=detail)
 
 
 class UnprocessableException(HTTPException):
-    def __init__(self, code='unprocessable_entity', detail: Any = None):
+    def __init__(self, code=ErrorCode.unprocessable_entity, detail: Any = None):
         super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, code=code, detail=detail)
 
 
 class TooManyRequestException(HTTPException):
-    def __init__(self, code='too_many_request', detail: Any = None):
-        super().__init__(status_code=status.HTTP_429_TOO_MANY_REQUESTS, code=code, detail=detail)
+    def __init__(self, detail: Any = None):
+        super().__init__(status_code=status.HTTP_429_TOO_MANY_REQUESTS, code=ErrorCode.too_many_requests, detail=detail)
 
 
 class InternalErrorException(HTTPException):
-    def __init__(self, code='internal_server_error', detail: Any = None):
-        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, code=code, detail=detail)
+    def __init__(self, detail: Any = None):
+        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         code=ErrorCode.internal_server_error, detail=detail)
 
 
 # 추후에 모든 익셉션을 처리할 수 있도록 Exception 타입을 받는 방식으로 만듬
@@ -69,5 +92,5 @@ async def custom_http_exception_handler(request: Request, exc: Exception) -> JSO
         return JSONResponse({"code": exc.code, "detail": exc.detail}, status_code=exc.status_code)
     else:
         logging.error(f"[{request_id}] {request.method} {request.url.path} request is failed. ", exc_info=True)
-        return JSONResponse({"code": 'internal_server_error', "detail": str(exc)},
+        return JSONResponse({"code": ErrorCode.internal_server_error, "detail": str(exc)},
                             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
