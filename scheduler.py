b5f1342f37eb6f91826b0dc73f47975bfb589dc7
Daniel Ji <bwjhj1030@gmail.com>
2020-05-09 01:30:07 +0900

    scheduler에 추가 & scheduler 도커파일 생성

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,6 @@
 import asyncio
+import logging
+from datetime import datetime
 
 import aiohttp
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -6,21 +8,27 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from config import conf
 
 __HOST = conf.get('HOST')
+logging.basicConfig(level=logging.INFO)
 
 
-async def crawl_voucher():
-    print(__HOST)
-    # async with aiohttp.ClientSession() as session:
-    #     async with session.post(f'{__HOST}/crawl_by_store_id') as res:
-    #         await res.text()
+async def log():
+    now = datetime.utcnow()
+    logging.info(f'scheduler is active at {now}')
+
+
+async def crawl_vouchers():
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'{__HOST}/vouchers/crawl') as res:
+            await res.text()
 
 
 async def get_scheduler():
     scheduler = AsyncIOScheduler()
-    # scheduler.add_job(crawl_voucher, 'cron', minute='*/20', second='10')
-    scheduler.add_job(crawl_voucher, 'cron', minute='*', second='*/2')
+    scheduler.add_job(log, 'cron', minute='0/5')
+    scheduler.add_job(crawl_vouchers, 'cron', minute='25/30', second='1')
     scheduler.start()
 
 if __name__ == '__main__':
+    logging.info(f'start viole scheduler with host: {__HOST} at {datetime.utcnow()}')
     asyncio.gather(get_scheduler())
     asyncio.get_event_loop().run_forever()
